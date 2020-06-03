02aece97df91b2dd21d172d3130da9ccbca23797
Matthew Richardson <M.Richardson@ed.ac.uk>
2020-06-03 14:15:56 +0100

    UYpdate base url. Add facebook ogp metadata.

--- push-ghpages.sh
+++ push-ghpages.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Make sure module is installed globally
 # yarn global add angular-cli-ghpages
-ng build --prod --base-href "https://mrichar1.github.io/beedentify/"
+ng build --prod --base-href "https://beedentify.io"
 ngh
 
