210ab6d4f9f4fddc927ac1be7de1e61430641199
Matthew Richardson <M.Richardson@ed.ac.uk>
2020-06-03 16:24:27 +0100

    Update push scritp.

--- push-ghpages.sh
+++ push-ghpages.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 # Make sure module is installed globally
 # yarn global add angular-cli-ghpages
-ng build --prod --base-href "https:/mrichar1.github.io/beedentify"
+# Don't use base-href when using beedentify.info URL
+ng build --prod # --base-href "https:/mrichar1.github.io/beedentify"
 
 # Fix for https://github.com/angular/angular/issues/30835
 sed -i -e 's#type="module"#type="text/javascript"#g' dist/beedentify/index.html
+
+# Preserve CNAME file
+cp -f CNAME dist/beedentify/
+
+# Push to gh-pages
 ngh --dir dist/beedentify
 
